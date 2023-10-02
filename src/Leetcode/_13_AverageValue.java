f3a922662ddd9382ee39c199f5e665d9dd7c126c
subas <subasmohanty256@gmail.com>
2023-10-02 15:11:39 +0530

    file name change according to naming convention of my package

--- src/Leetcode/_13_AverageValue.java
+++ src/Leetcode/_13_AverageValue.java
@@ -1,13 +1,12 @@
 package Leetcode;
 
-public class AverageValue {
+public class _13_AverageValue {
     public int averageValue(int[] nums) {
         if(nums.length==0){
             return 0;
         }
         int count=0;
         int sum=0;
-
         for(int i=0;i<nums.length;i++){
             // The number which is divided by 6 is divided by both 2 and 3 so it is even as well as divisible by 3
             if(nums[i]%6==0){

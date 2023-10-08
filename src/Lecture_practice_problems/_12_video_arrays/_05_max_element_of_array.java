78be88db3fc538e3bebfd3e418c5d3d66ae42402
subas <subasmohanty256@gmail.com>
2023-10-08 16:00:31 +0530

    a new concept added

--- src/Lecture_practice_problems/_12_video_arrays/_05_max_element_of_array.java
+++ src/Lecture_practice_problems/_12_video_arrays/_05_max_element_of_array.java
@@ -1,12 +1,16 @@
 package Lecture_practice_problems._12_video_arrays;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class _05_max_element_of_array {
     public static void main(String[] args) {
         int [] arr={45,54,12,78,178};
         System.out.println(max(arr));
         System.out.println(maxInRange(arr,1,3));
+
+        // this will sort the array in reverse order but the arr should be of wrapper class, i.e, Integer [] arr= new Integer[];
+        //Arrays.sort(arr, Collections.reverseOrder());
     }
 
     // check the maximum value in the given range of the array

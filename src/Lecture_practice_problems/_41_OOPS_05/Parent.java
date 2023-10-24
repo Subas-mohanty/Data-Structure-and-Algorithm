eb89e401bb71c0a92b6d1c386bb998bc41add154
subas <subasmohanty256@gmail.com>
2023-10-24 17:41:48 +0530

    abstract class and interface revised

--- src/Lecture_practice_problems/_41_OOPS_05/Parent.java
+++ src/Lecture_practice_problems/_41_OOPS_05/Parent.java
@@ -3,8 +3,9 @@ package Lecture_practice_problems._41_OOPS_05;
 public class Parent extends AbstractClass {
     public static void main(String[] args) {
         Parent parent=new Parent();
-        AbstractClass pita=new Parent();
+        AbstractClass pita=new Parent(); // abstract class reference and Parent class object (dynamic method dispatch)
         AbstractClass.myName();
+//        AbstractClass abs=new AbstractClass(); // can't be done
     }
     static void hii(){
         System.out.println("Hii this is me !");

20c8caca932d31bef8b9f305a62891c23aa67c4d
subas <subasmohanty256@gmail.com>
2023-11-02 21:23:59 +0530

    Leetcode problem : 921. Minimum Add to Make Parentheses Valid, solved

---
+++ src/Lecture_practice_problems/_47_Stack_and_Queues_questions/_05_validParenthesis_MinAdd.java
@@ -0,0 +1,22 @@
+package Lecture_practice_problems._47_Stack_and_Queues_questions;
+
+import java.util.Stack;
+
+public class _05_validParenthesis_MinAdd {
+
+    public int dj(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == ')') {
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.size();
+    }
+}

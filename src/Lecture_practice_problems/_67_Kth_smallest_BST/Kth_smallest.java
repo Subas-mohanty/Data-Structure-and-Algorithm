e2dff9b704618dcbaec74775c6aed3de72bda544
subas <subasmohanty256@gmail.com>
2023-12-15 13:32:27 +0530

    Leetcode(653) - Kth smallest element in BST

---
+++ src/Lecture_practice_problems/_67_Kth_smallest_BST/Kth_smallest.java
@@ -0,0 +1,42 @@
+package Lecture_practice_problems._67_Kth_smallest_BST;
+
+import java.util.PriorityQueue;
+
+//https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+public class Kth_smallest {
+    public class TreeNode {
+     int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public int kthSmallest(TreeNode node, int k) {
+        int ans=0;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        helper(node,k,minHeap);
+
+        //for kth element
+        for(int i=0;i<k;i++){
+            ans=minHeap.poll();
+        }
+        return ans;
+
+    }
+    // inorder traversal to sort the BST, and store the values in a minHeap
+    private void helper(TreeNode node, int k, PriorityQueue<Integer> minHeap){
+        if(node==null){
+            return;
+        }
+        helper(node.left,k,minHeap);
+
+        minHeap.offer(node.val);
+
+        helper(node.right,k,minHeap);
+    }
+}
---
+++ src/Lecture_practice_problems/_67_Kth_smallest_BST/Kth_smallest2.java
@@ -0,0 +1,39 @@
+package Lecture_practice_problems._67_Kth_smallest_BST;
+
+//https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+public class Kth_smallest2 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    private int k;
+    private int ans=0;
+    public int kthSmallest(TreeNode node, int k) {
+        this.k=k;
+        helper(node);
+        return ans;
+    }
+    // inorder traversal to sort the BST, and store the values in a minHeap
+    private void helper(TreeNode node){
+        if(node==null){
+            return;
+        }
+        helper(node.left);
+        // after reaching at the smallest element we started reducing the k value with 1 so that when the k value will be 0 , we will be at our answer
+        k--;
+        if(k==0){
+            ans=node.val;
+        }
+
+        helper(node.right);
+    }
+}

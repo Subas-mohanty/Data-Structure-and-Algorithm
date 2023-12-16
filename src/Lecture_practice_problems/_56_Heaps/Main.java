a2de10d75140435e160daa8f61268a65b6c5613c
subas <subasmohanty256@gmail.com>
2023-12-16 08:24:57 +0530

    Heap revised

--- src/Lecture_practice_problems/_56_Heaps/Main.java
+++ src/Lecture_practice_problems/_56_Heaps/Main.java
@@ -1,6 +1,8 @@
 package Lecture_practice_problems._56_Heaps;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws Exception{
@@ -15,7 +17,21 @@ public class Main {
 //        System.out.println(heap.remove());
 //        System.out.println(heap.remove());
 //        System.out.println(heap.remove());
-        ArrayList<Integer> list = heap.heapSort();
-        System.out.println(list);
+//        ArrayList<Integer> list = heap.heapSort();
+//        System.out.println(list);
+
+        // maxHeap
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        // minHeap
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        minHeap.offer(3);
+        minHeap.offer(32);
+        minHeap.offer(31);
+        minHeap.offer(13);
+        minHeap.offer(233);
+        int size = minHeap.size();
+        for (int i = 0; i < size; i++) { // we can't do minHeap.size() directly here as it is changing after every iteration hence all items will not be printed
+            System.out.println(minHeap.poll());
+        }
     }
 }

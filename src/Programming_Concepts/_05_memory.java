224cf0da5f64bf9a41a5377819d6607f52fbb5c3
subas <subasmohanty256@gmail.com>
2023-12-25 01:58:50 +0530

    LeetCode(124) - BT maximum path sum

---
+++ src/Programming_Concepts/_05_memory.java
@@ -0,0 +1,8 @@
+package Programming_Concepts;
+
+public class _05_memory {
+    public static void main(String[] args) {
+        int [] arr = new int[5];
+        System.out.println(arr[2]);
+    }
+}

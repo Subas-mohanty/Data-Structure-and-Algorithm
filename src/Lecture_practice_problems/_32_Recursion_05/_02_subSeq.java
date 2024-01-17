b35c6b577d21bbef749c6bc5d31d8001f49ef54e
subas <subasmohanty256@gmail.com>
2024-01-18 03:16:19 +0530

    Lec-32 Recursion-05 subsets and subsequence revision done

--- src/Lecture_practice_problems/_32_Recursion_05/_02_subSeq.java
+++ src/Lecture_practice_problems/_32_Recursion_05/_02_subSeq.java
@@ -9,28 +9,38 @@ public class _02_subSeq {
 //        ArrayList<String> ans=subSeqRet("","abc");
 //        System.out.println(ans);
 
-        subSeqAscii("","abc");
+      subSeqAscii("","abc");
     }
     static void subSeq(String p,String up){
-        if (up.isEmpty()){
-            System.out.println(p);
-            return;
-        }
-        char ch=up.charAt(0);
-        subSeq(p+ch,up.substring(1));
-        subSeq(p,up.substring(1));
+      if (up.isEmpty()){
+        System.out.println(p);
+        return;
+      }
+      char ch=up.charAt(0);
+      subSeq(p+ch,up.substring(1));
+      subSeq(p,up.substring(1));
     }
-
+    static ArrayList<String> subseq(String text, String ans, ArrayList<String> list){
+      if(text.isEmpty()){
+        list.add(ans);
+        return list;
+      }
+      char ch = text.charAt(0);
+      subseq(text.substring(1), ans+ch, list);
+      subseq(text.substring(1), ans, list);
+      return list;
+    }
     static ArrayList<String> subSeqRet(String p, String up){
-        if (up.isEmpty()){
-            ArrayList<String> list=new ArrayList<>();
-            list.add(p);
-            return list;
-        }
-        char ch=up.charAt(0);
-        ArrayList<String> left =subSeqRet(p+ch,up.substring(1));
-        ArrayList<String> right =subSeqRet(p,up.substring(1));
+      if (up.isEmpty()){
+        ArrayList<String> list=new ArrayList<>();
+        list.add(p);
+        return list;
+      }
+      char ch=up.charAt(0);
+      ArrayList<String> left =subSeqRet(p+ch,up.substring(1));
+      ArrayList<String> right =subSeqRet(p,up.substring(1));
 
+      //combining left and right
         left.addAll(right);
         return left;
     }
--- src/Lecture_practice_problems/_32_Recursion_05/_03_Subsets.java
+++ src/Lecture_practice_problems/_32_Recursion_05/_03_Subsets.java
@@ -1,8 +1,9 @@
 package Lecture_practice_problems._32_Recursion_05;
 //https://leetcode.com/problems/subsets/
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
+// taking some elements and removing some elements
 public class _03_Subsets {
     public static void main(String[] args) {
         int [] arr={1,2,3};
@@ -11,17 +12,50 @@ public class _03_Subsets {
             System.out.println(list);
         }
     }
+    // TC : O(N*2^N) --> No. of elements * total no. of subsets
+    // SC : O(N*2^N) --> size of each list * no of list in the outer/answer array(no. of subsets)
     static List<List<Integer>> subset(int [] arr){
         List<List<Integer>> outer=new ArrayList<>();
-
+        // adding an empty list to the outer list
         outer.add(new ArrayList<>());
 
+        // for every number we are running the inside loop the number of times, the number of elements the outer list has
         for(int num:arr){
             int n=outer.size();
             for (int i = 0; i < n; i++) {
+                // copying the previous elements with the constructor and adding the number in each element
                 List<Integer> internal=new ArrayList<>(outer.get(i));
                 internal.add(num);
                 outer.add(internal);
+            }
+        }
+        return outer;
+    }
+    // when we find a duplicate element, only add it in the newly created subset of previous steps
+    // because of above point duplicates needs to be together so sort the array
+    static ArrayList<ArrayList<Integer>> subset2(int [] arr){
+        Arrays.sort(arr);
+        ArrayList<ArrayList<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        int start =0;
+        int end =0;
+
+        for(int i =0; i<arr.length; i++){
+            start =0;
+            int n = outer.size();
+            // found a duplicate means the current and the previous element is same
+            if(i > 0 && arr[i] == arr[i-1]){
+                start= end+1;
+            }
+            // as the previous if will not going to work for i=0 or for the first element , so this will be the end = 2-1=1, then we will end the next element from the 1st index
+            // look what we are doing is , when we found a duplicate element we are not adding it from the start as we were doing before instead we start adding the number from the previous end or after the index till where that element has already been added due to its first occurrence
+            end = outer.size() -1;
+            for(int j=start; j< n; j++){
+                ArrayList<Integer> temp = new ArrayList<>(outer.get(j));
+
+                temp.add(arr[i]);
+                outer.add(temp);
             }
         }
         return outer;

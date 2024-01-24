679dcc7f9df6374c5bfe9a5efb77b1232fe989e7
subas <subasmohanty256@gmail.com>
2024-01-24 20:48:05 +0530

    Lec-34 Recursion-07 revision done

--- src/Lecture_practice_problems/_34_Recursion_07/_02_Dice.java
+++ src/Lecture_practice_problems/_34_Recursion_07/_02_Dice.java
@@ -4,9 +4,23 @@ import java.util.ArrayList;
 
 public class _02_Dice {
     public static void main(String[] args) {
-        System.out.println(dice("",4));
+        dice("",3);
+//        System.out.println(diceCount("",4));
+        ArrayList<ArrayList<String>> list = dice3("", 4);
+        for(ArrayList<String> ans : list){
+//            System.out.println(ans);
+        }
     }
-    static ArrayList<String> dice(String p, int target){
+    static void dice(String ans, int target){
+        if(target == 0){
+            System.out.println(ans);
+            return;
+        }
+        for(int i =1; i<= 6 && i <= target; i++){
+            dice(ans+i, target - i);
+        }
+    }
+    static ArrayList<String> dice2(String p, int target){
         if (target == 0) {
             ArrayList<String> list=new ArrayList<>();
             list.add(p);
@@ -14,7 +28,7 @@ public class _02_Dice {
         }
         ArrayList<String> ans=new ArrayList<>();
         for (int i = 1; i <= target ; i++) {
-            ans.addAll(dice(p+i,target-i));
+            ans.addAll(dice2(p+i,target-i));
         }
         return ans;
     }
@@ -24,10 +38,22 @@ public class _02_Dice {
         }
         int count=0;
         for (int i = 1; i <=6 && i<=target ; i++) {
-
-            diceCount(p+i,target-i);
-            count++;
+            count +=diceCount(p+i,target-i);
         }
         return count;
+    }
+    static ArrayList<ArrayList<String>> dice3(String ans, int target){
+        ArrayList<ArrayList<String>> outer = new ArrayList<>();
+        if(target == 0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(ans);
+            outer.add(list);
+            return outer;
+        }
+        for(int i =1; i<= 6 && i <= target; i++){
+            ArrayList<ArrayList<String>> res = dice3(ans+i, target - i);
+            outer.addAll(res);
+        }
+        return outer;
     }
 }

d64756ee195c366e1a6ea403af0d56a121c6b3b8
subas <subasmohanty256@gmail.com>
2024-01-31 18:23:02 +0530

    House Robber 2 problem done

--- src/Dynamic_Programming_Striver/_06_MaxSum_Non_adjacent.java
+++ src/Dynamic_Programming_Striver/_06_MaxSum_Non_adjacent.java
@@ -3,15 +3,19 @@ package Dynamic_Programming_Striver;
 import java.util.Arrays;
 //https://leetcode.com/problems/house-robber/description/
 public class _06_MaxSum_Non_adjacent {
-    public int rob(int[] nums) {
-        int [] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return helper(nums.length - 1, nums, dp);
-    }
-    int helper(int index, int[] arr, int [] dp){
-        // if index is 0 that means we have not taken index 1 so we have to add arr[index] in the answer
-        if(index == 0) return arr[0];
-        if(index == -1) return 0;
+  public static void main(String[] args) {
+    int [] arr = {1,4,3,4,5,2,9};
+    System.out.println(helper3(arr));
+  }
+  public int rob(int[] nums) {
+    int [] dp = new int[nums.length];
+    Arrays.fill(dp, -1);
+    return helper(nums.length - 1, nums, dp);
+  }
+  int helper(int index, int[] arr, int [] dp){
+    // if index is 0 that means we have not taken index 1 so we have to add arr[index] in the answer
+    if(index == 0) return arr[0];
+    if(index == -1) return 0;
 
         if(dp[index] != -1) return dp[index];
 
---
+++ src/Dynamic_Programming_Striver/_07_House_Robber2.java
@@ -0,0 +1,55 @@
+package Dynamic_Programming_Striver;
+import java.util.Arrays;
+//https://leetcode.com/problems/house-robber-ii/description/
+
+public class _07_House_Robber2 {
+  public static void main(String[] args) {
+    int [] arr= {1,4,7,5,8,6,2,1};
+    System.out.println(rob2(arr));
+    // the only thing that we need to change in 6th maxSum is here we can't take first and last element together, so first we take the first element and ignore the last and calculate the maxSum then again we ignore the first one and take the last one and calculate the maxSum.
+    // Now for the answer we take max of these two
+    int [] first = Arrays.copyOfRange(arr, 1, 8);
+    int [] second = Arrays.copyOfRange(arr, 0, 7);
+    int ans = Math.max(rob(first), rob(second));
+    System.out.println(ans);
+  }
+  public static int rob(int [] arr) {
+    int n = arr.length;
+    int prev = arr[0];
+    int prev2 = 0;
+    for (int i = 1; i < n; i++) {
+      int pick = arr[i];
+      if(i > 1) pick += prev2;
+      int notPick = 0 + prev;
+      int ans = Math.max(pick, notPick);
+      prev2 = prev;
+      prev = ans;
+    }
+    return prev;
+  }
+  public static int rob2(int [] arr) {
+    int n = arr.length;
+    int prev = arr[0];
+    int prev2 = 0;
+    for (int i = 1; i < n - 1; i++) {
+      int pick = arr[i];
+      if(i > 1) pick += prev2;
+      int notPick = 0 + prev;
+      int ans = Math.max(pick, notPick);
+      prev2 = prev;
+      prev = ans;
+    }
+
+    int prev3 = arr[1];
+    int prev4 = 0;
+    for (int i = 2; i < n; i++) {
+      int pick = arr[i];
+      if(i > 2) pick += prev4;
+      int notPick = 0 + prev3;
+      int ans = Math.max(pick, notPick);
+      prev4 = prev3;
+      prev3 = ans;
+    }
+    return Math.max(prev, prev3);
+  }
+}

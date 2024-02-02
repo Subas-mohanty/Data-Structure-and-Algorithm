5a6ce9999cf4b44ab3cd687bda8acaf066d8d850
subas <subasmohanty256@gmail.com>
2024-02-02 20:28:46 +0530

    dp-08 ninja training memoization

--- src/Dynamic_Programming_Striver/_08_Ninja_Training.java
+++ src/Dynamic_Programming_Striver/_08_Ninja_Training.java
@@ -1,35 +1,42 @@
 package Dynamic_Programming_Striver;
+import java.util.Arrays;
 
 public class _08_Ninja_Training {
-    public static void main(String[] args) {
-        int [][] points = {
-                {1,2,5},
-                {3,1,1},
-                {3,3,3},
-        };
-        System.out.println(ninjaTraining(points.length, points));
+  public static void main(String[] args) {
+    int [][] points = {
+      {10,40,70},
+      {20,50,80},
+      {30,60,90},
+    };
+    System.out.println(ninjaTraining(points.length, points));
+  }
+  public static int ninjaTraining(int n, int[][] points) {
+    int [][] dp = new int[n+1][4];
+    for(int [] row : dp){
+      Arrays.fill(row, -1);
     }
-    public static int ninjaTraining(int n, int points[][]) {
-        int [][] dp = new int[n+1][4];
-        return helper(n-1, points, 3, dp);
-    }
-    public static int helper(int n, int [][] points , int last, int [][] dp){
-        if(n==0){
-            int max =0;
-            for(int i =0 ; i< 3; i++){
-                if(i != last){
-                    max = Math.max(max, points[n][i]);
-                }
-            }
-            return max;
+    return helper(n-1, points, 3, dp);
+  }
+  public static int helper(int n, int [][] points , int last, int [][] dp){
+    if(n==0){
+      int max =0;
+      for(int i =0 ; i< 3; i++){
+        if(i != last){
+          max = Math.max(max, points[n][i]);
         }
-        int ans = 0;
-        for(int i =0; i< 3; i++){
-            if(i != last){
-                int point = points[n][i] + helper(n-1, points, i, dp);
-                ans= Math.max(point, ans);
-            }
-        }
-        return ans;
+      }
+      return max;
     }
+    int ans = 0;
+    for(int i =0; i< 3; i++){
+      if(i != last && dp[n][last] != -1){
+        return dp[n][last];
+      }
+      if(i != last){
+        int point = points[n][i] + helper(n-1, points, i, dp);
+        ans= Math.max(point, ans);
+      }
+    }
+    return dp[n][last] = ans;
+  }
 }

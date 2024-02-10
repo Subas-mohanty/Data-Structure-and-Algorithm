51a5b0b8aaf6a3ae43b4f03b86eb950d553991b5
subas <subasmohanty256@gmail.com>
2024-02-10 18:56:33 +0530

    dp-12 triangle sum tabulation done

--- src/Dynamic_Programming_Striver/_12_Triangle_min_pathSum.java
+++ src/Dynamic_Programming_Striver/_12_Triangle_min_pathSum.java
@@ -16,7 +16,8 @@ public class _12_Triangle_min_pathSum{
         for(int [] arr: dp){
             Arrays.fill(arr, Integer.MIN_VALUE);
         }
-        System.out.println(sum(grid, 0, 0, dp));
+//        System.out.println(sum(grid, 0, 0, dp));
+        System.out.println(sum2(grid, m, n, dp));
     }
     public static int sum(int [][] grid, int row, int col, int [][] dp){
         if(row == grid.length-1){
@@ -31,5 +32,19 @@ public class _12_Triangle_min_pathSum{
         if(row+1 < grid.length && col < grid[row+1].length-1) diag = grid[row][col] + sum(grid, row + 1, col + 1, dp);
 
         return dp[row][col] = Math.min(down, diag);
+    }
+    // tabulation doesn't mean always starts from 0 , instead if the recursion is starting from 0 then the tabulation should be start from length-1, it will always be opposite to recursion because recursion is always top down
+    public static int sum2(int [][] grid, int row, int col, int [][] dp){
+        for (int i = 0; i < row; i++) {
+            dp[row-1][i] = grid[row-1][i];
+        }
+        for (int i = row-2; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+                    int down = grid[i][j] + dp[i+1][j];
+                    int diag = grid[i][j] + dp[i+1][j+1];
+                    dp[i][j] = Math.min(down, diag);
+            }
+        }
+        return dp[0][0];
     }
 }

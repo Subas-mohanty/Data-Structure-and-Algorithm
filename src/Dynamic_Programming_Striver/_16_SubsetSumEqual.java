1ba272c531399a9231933c5ab173cb0982fe33a3
subas <subasmohanty256@gmail.com>
2024-02-16 19:18:48 +0530

    dp-16 partition equal subset sum done

--- src/Dynamic_Programming_Striver/_16_SubsetSumEqual.java
+++ src/Dynamic_Programming_Striver/_16_SubsetSumEqual.java
@@ -1,36 +1,34 @@
 package Dynamic_Programming_Striver;
 
 import java.util.Arrays;
+//https://bit.ly/34iIIsH
 
-//https://www.codingninjas.com/studio/problems/subset-sum-equal-to-k_1550954?interviewProblemRedirection=true
-public class _15_SubsetSum {
+public class _16_SubsetSumEqual {
   public static void main(String[] args) {
-    int [] arr = {1,2,3,4};
-    int target = 11;
+    int [] arr = {1,2,23,4};
+
+    // finding the sum of all the elements of the array
+    int sum = 0;
+    for (int num : arr) {
+      sum+= num;
+    }
+    // The question states that whether the array can be divided into two subsets whose sum will be equal, that means the two subsets will have sum equal to total sum of the array divided by 2, so if the sum is odd then the ans will be false as we can't divide it into two parts else the target will be sum/2, and it can be solved by the previous approach used in _15_SubsetSum.java
+    int target = sum/2;
     int n = arr.length;
 
     int [][] dp = new int[n][target+1];
     for(int [] temp : dp){
       Arrays.fill(temp, -1);
     }
-    System.out.println(findSum(arr, n-1, target, dp));
-//    System.out.println(false | false); // we can use bitwise OR instead of logical OR in this case
-  }
-  // Normal Recursion
-  // TC : O(2^n) as every element in the array has 2 option
-  // SC : O(n) auxiliary space(extra space) for the recursion stack space
-  public static boolean findSum(int [] arr, int index, int target){
-    if(target == 0){
-      return true;
+    if((sum & 1) == 1){
+      System.out.println(false);
     }
-    if(index == 0 ) return target == arr[index] ;
-
-    boolean pick = false;
-    if(arr[index] <= target) pick = findSum(arr, index-1, target - arr[index]);
-    boolean notPick = findSum(arr, index - 1, target);
-
-    return pick || notPick;
+    else {
+      System.out.println(findSum(arr, n-1, target, dp));
+    }
+//    (sum & 1) == 1 ? return false : return findSum(arr, n -1 , target, dp); we can't do like this , because returning a function using a ternary operator is not allowed in java
   }
+
   // memoization
   // TC : (n*k)
   // SC : O(n) + O(n*k) recursion stack space + the dp array
@@ -44,6 +42,7 @@ public class _15_SubsetSum {
     if(dp[index][target] != -1){
       return dp[index][target] == 0 ? false : true ; // if dp[index][target] == 0 means false value is stored at that index otherwise it is true
     }
+
     boolean pick = false;
     // if the element at the index is greater than the target than there is no use of taking that element into consideration
     if(arr[index] <= target) pick = findSum(arr, index-1, target - arr[index], dp);

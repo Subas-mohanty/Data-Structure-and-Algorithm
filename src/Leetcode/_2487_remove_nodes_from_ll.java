9c59df948ca66fce8314545f10b249e9e8c7361a
subas <subasmohanty256@gmail.com>
2024-05-08 20:16:46 +0530

    remove nodes from LL done

---
+++ src/Leetcode/_2487_remove_nodes_from_ll.java
@@ -0,0 +1,23 @@
+package Leetcode
+
+// https://leetcode.com/problems/remove-nodes-from-linked-list/description/?envType=daily-question&envId=2024-05-06
+public class _2487_remove_nodes_from_ll {
+  public ListNode removeNodes(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        ListNode temp = head;
+
+        // monotonic decreasing stack, that means it will only have strictly decreasing values
+        while(temp != null){
+            while(!stack.isEmpty() && temp.val > stack.peek().val) stack.pop();
+            stack.push(temp);
+            temp = temp.next;
+        }
+        ListNode end = null;
+        while(!stack.isEmpty()){
+            temp = stack.pop();
+            temp.next = end;
+            end = temp;
+        }
+        return temp;
+    }
+}

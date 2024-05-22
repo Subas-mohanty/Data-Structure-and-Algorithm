2196a04e6b0636dc35768c85f117273445f63741
subas <subasmohanty256@gmail.com>
2024-05-23 00:18:22 +0530

    sum of beauty of substring problem done

--- src/Dynamic_Programming_Striver/_35_wildcard_matching.java
+++ src/Dynamic_Programming_Striver/_35_wildcard_matching.java
@@ -18,6 +18,92 @@ public class _35_wildcard_matching {
         boolean [][] Dp = new boolean[i+1][j+1];
         System.out.println(match3(s1, s2, Dp));
     }
+
+
+    // for leetcode we have to modify it a little bit
+    public static boolean isMatch(String s, String p) {
+        return match2(s, p, s.length() -1 , p.length() -1);
+    }
+    public static boolean match2(String s1, String s2, int i, int j){
+        if(i<0 && j<0) return true;
+
+        if(i<0){
+            for (int k = 0; k <= j; k++) {
+                if(s2.charAt(k) != '*') return false;
+            }
+            return true;
+        }
+
+        if(j<0){
+            for (int k = 0; k <= i; k++) {
+                if(s1.charAt(k) != '*') return false;
+            }
+            return true;
+        }
+        if(s1.charAt(i) == s2.charAt(j) || s1.charAt(i) == '?' || s2.charAt(j) == '?') return match2(s1, s2, i-1, j-1);
+        if(s1.charAt(i) == '*' || s2.charAt(j) == '*'){
+            return match2(s1, s2, i-1,j) || match2(s1, s2, i, j-1);
+        }
+        return false;
+    }
+    public static boolean match(String s1, String s2, int i, int j, boolean [][] dp){
+        if(i<0 && j<0) return true;
+
+        if(i<0){
+            for (int k = 0; k <= j; k++) {
+                if(s2.charAt(k) != '*') return false;
+            }
+            return true;
+        }
+        if(j<0) return false; // if pattern string is over and main string is not over then we can't match it
+
+        if(dp[i][j]) return dp[i][j];
+
+        if(s1.charAt(i) == s2.charAt(j) || s1.charAt(i) == '?' || s2.charAt(j) == '?'){
+            dp[i][j] = match(s1, s2, i-1, j-1, dp);
+        }
+        else if(s1.charAt(i) == '*' || s2.charAt(j) == '*'){
+            dp[i][j] = match(s1, s2, i-1,j, dp) || match(s1, s2, i, j-1, dp);
+        }
+        return dp[i][j];
+    }
+
+    public static boolean match(String s1, String s2, boolean [][] dp){
+        int len_s = s1.length();
+        int len_p = s2.length();
+
+        dp[0][0] = true;
+        for(int i = 1 ; i <= len_s; i++) dp[i][0] = false;
+        for(int i = 1; i <= len_p; i++){
+            if(s2.charAt(i-1) == '*') dp[0][i] = dp[0][i-1];
+        }
+
+        for(int i = 1; i <= len_s; i++){
+            for(int j = 1; j <= len_p; j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1) || s1.charAt(i-1) == '?' || s2.charAt(j-1) == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(s1.charAt(i-1) == '*' || s2.charAt(j-1) == '*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+            }
+        }
+        return dp[len_s][len_p];
+    }
+
+
+
+
+
+
+
+
+    
+
+
+
+
+    // code by striver
     public static boolean match(String s1, String s2, int i, int j){
         if(i<0 && j<0) return true; // if both the string got exhausted which means we got a match
         if(i<0 && j>=0) return false; // s1 is exhausted but s2 is not so there is no match
---
+++ src/Leetcode/_1781_sum_of_beauty_of_substring.java
@@ -0,0 +1,49 @@
+package Leetcode;
+//https://leetcode.com/problems/sum-of-beauty-of-all-substrings/
+public class _1781_sum_of_beauty_of_substring {
+    // TC : O(n^2) * 26
+    // SC : O(26) || O(1)
+    public int beautySum(String s) {
+        int ans = 0;
+        int n = s.length();
+        for(int i = 0; i < n; i++){
+            int [] fr = new int[26]; // storing the frequency of characters
+            for(int j = i; j < n; j++){
+                fr[s.charAt(j) - 'a']++; // incrementing the frequency
+                ans += freqDiff(fr);
+            }
+        }
+        return ans;
+    }
+
+    // this is finding the maximum and minimum frequency of the array, or the difference between the maximum and the minimum element in the array
+    public int freqDiff(int [] fr){
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i < fr.length; i++){
+            // ignoring 0, because if we take 0, it may result to wrong answer
+            // but how can 0 be present in the array as we are updating according to every character, here is an example
+             /* For i = 0:
+
+            j = 0: Substring = "a", fr = [1, 0, 0, ..., 0]
+            j = 1: Substring = "ab", fr = [1, 1, 0, ..., 0]
+            j = 2: Substring = "abc", fr = [1, 1, 1, ..., 0]
+            For i = 1:
+
+            j = 1: Substring = "b", fr = [0, 1, 0, ..., 0]
+            j = 2: Substring = "bc", fr = [0, 1, 1, ..., 0]
+            For i = 2:
+
+            j = 2: Substring = "c", fr = [0, 0, 1, ..., 0]
+
+            */
+            if(fr[i] == 0) continue;
+
+            min = Math.min(min, fr[i]);
+            max = Math.max(max, fr[i]);
+        }
+
+        return max-min;
+    }
+}

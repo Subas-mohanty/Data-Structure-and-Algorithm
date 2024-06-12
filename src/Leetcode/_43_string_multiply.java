5ac28b3d47e46a1d4210301a86f92e88f1a2983e
subas <subasmohanty256@gmail.com>
2024-06-12 22:24:24 +0530

    string multiply problem done

---
+++ src/Leetcode/_43_string_multiply.java
@@ -0,0 +1,26 @@
+package Leetcode;
+//https://leetcode.com/problems/multiply-strings/description/
+public class _43_string_multiply {
+    public String multiply(String num1, String num2) {
+        int m = num1.length(), n = num2.length();
+        int[] pos = new int[m + n];
+
+        for(int i = m - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+
+                // p2 is the position of the carry
+                int p1 = i + j, p2 = i + j + 1;
+                int sum = mul + pos[p2];
+
+                pos[p1] += sum / 10;
+                pos[p2] = (sum) % 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        // the if condition checks if the first number is 0, then don't add it in the sb
+        for(int p : pos) if(!(sb.length() == 0 && p == 0)) sb.append(p);
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}

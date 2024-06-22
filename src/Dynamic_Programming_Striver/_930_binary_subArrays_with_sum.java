a68f0b337fe2c7421c7e818b5f841b07c2dce6e6
subas <subasmohanty256@gmail.com>
2024-06-22 23:30:54 +0530

    gcd of string problem done

--- src/Dynamic_Programming_Striver/_930_binary_subArrays_with_sum.java
+++ src/Dynamic_Programming_Striver/_930_binary_subArrays_with_sum.java
@@ -7,6 +7,51 @@ public class _930_binary_subArrays_with_sum {
         int goal = 2;
         System.out.println(numSubarraysWithSum(arr, goal));
     }
+
+    public static int numSubarraysWithSum2(int[] nums, int goal) {
+        // find number of subArray where sum <= goal and where sum <= goal -1 , when subtract these two we will get the number of subArrays in which sum == goal
+        return sumAtMostK(nums, goal) - sumAtMostKminus1(nums, goal);
+    }
+    public static int sumAtMostK(int[] nums, int goal) {
+        int s = 0;
+        int e = 0;
+        int n = nums.length;
+        int sum = 0;
+        int count = 0;
+        while(e < n){
+            sum += nums[e];
+            while(s <= e && sum > goal) {
+                sum -= nums[s];
+                s++;
+            }
+            if(sum <= goal) count += (e-s+1);
+            e++;
+        }
+        return count;
+    }
+    public static int sumAtMostKminus1(int[] nums, int goal) {
+        int s = 0;
+        int e = 0;
+        int n = nums.length;
+        int sum = 0;
+        int count = 0;
+        goal = goal-1;
+        while(e < n){
+            sum += nums[e];
+            while(s <= e && sum > goal) {
+                sum -= nums[s];
+                s++;
+            }
+            if(sum <= goal) count += (e-s+1);
+            e++;
+        }
+        return count;
+    }
+
+
+
+
+
     public static int numSubarraysWithSum(int[] nums, int goal) {
         int i = 0;
         int j = 0;
---
+++ src/Leetcode/_1071_GCD_of_strings.java
@@ -0,0 +1,42 @@
+package Leetcode;
+//https://leetcode.com/problems/greatest-common-divisor-of-strings/description/
+public class _1071_GCD_of_strings {
+
+    // SC : O(n)
+    // TC : O(n)
+        public String gcdOfStrings2(String str1, String str2) {
+            // Check if concatenated strings are equal or not, if not return ""
+            if (!(str1 + str2).equals(str2 + str1))
+                return "";
+            // If strings are equal than return the substring from 0 to gcd of size(str1), size(str2)
+            int gcd = gcd(str1.length(), str2.length());
+            return str1.substring(0, gcd);
+        }
+
+        // this is to find the gcd of two numbers
+        private int gcd(int a, int b) {
+            return b == 0 ? a : gcd(b, a % b);
+        }
+
+    public String gcdOfStrings(String str1, String str2) {
+        // Check if concatenated strings are equal or not, if not return ""
+        if (!(str1 + str2).equals(str2 + str1)) return "";
+        int m = str1.length();
+        int n = str2.length();
+        int i = 0;
+        int j = 0;
+        StringBuilder sb = new StringBuilder();
+        while(i < m && j < n){
+            if(str1.charAt(i) == str2.charAt(j)){
+                sb.append(str1.charAt(i));
+            }
+            i++;
+            j++;
+        }
+        while(!sb.isEmpty()){
+            if(m % sb.length() == 0 && n % sb.length() == 0) return sb.toString();
+            else sb.deleteCharAt(sb.length() -1);
+        }
+        return sb.toString();
+    }
+}

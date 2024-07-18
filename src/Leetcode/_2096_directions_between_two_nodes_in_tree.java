919e6c49df1076b8e42790b10de1c1c94ecd72ae
subas <subasmohanty256@gmail.com>
2024-07-18 15:16:25 +0530

    directions between two nodes problem done

---
+++ src/Leetcode/_2096_directions_between_two_nodes_in_tree.java
@@ -0,0 +1,56 @@
+package Leetcode;
+//https://shorturl.at/lDRMm
+public class _2096_directions_between_two_nodes_in_tree {
+    public String getDirections(TreeNode root, int startValue, int destValue) {
+
+        // sortest path in tree will go through the LCA(lowest common ancestors)
+        // finding the length from lca to start and the path from lca to desti
+        // then adding "U", length(lca to start) number of times
+        // appending path from lca to desti to the ans
+
+        StringBuilder path_to_s = new StringBuilder();
+        StringBuilder path_to_d = new StringBuilder();
+        TreeNode lca = LCA(root, startValue, destValue);
+        findPath(lca, startValue, path_to_s);
+        findPath(lca, destValue, path_to_d);
+        int length = path_to_s.length();
+
+        StringBuilder ans = new StringBuilder();
+        for(int i = 0; i < length; i++){
+            ans.append("U");
+        }
+        // for(int i = 0; i < path_to_d.length(); i++){
+        //     ans.append(path_to_d.charAt(i));
+        // }
+        ans.append(path_to_d);
+        return ans.toString();
+    }
+    public boolean findPath(TreeNode root, int target, StringBuilder sb){
+        if(root == null) return false;
+        if(root.val == target) return true;
+
+        sb.append("L");
+        if(findPath(root.left, target, sb)) return true;
+        sb.setLength(sb.length() - 1); // deleting last character
+
+        sb.append("R");
+        if(findPath(root.right, target, sb)) return true;
+        sb.setLength(sb.length() - 1); // deleting last character
+
+        return false;
+    }
+
+
+    public TreeNode LCA(TreeNode root, int p, int q){
+        if(root == null || root.val == p || root.val == q){
+            return root;
+        }
+        TreeNode left = LCA(root.left, p, q);
+        TreeNode right = LCA(root.right, p, q);
+
+        if(left != null && right != null){
+            return root;
+        }
+        return left == null? right : left;
+    }
+}
--- src/Leetcode/_2196_create_binary_tree_from_array.java
+++ src/Leetcode/_2196_create_binary_tree_from_array.java
@@ -43,18 +43,4 @@ public class _2196_create_binary_tree_from_array {
         }
         return null;
     }
-
-
-    public class TreeNode {
-        int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-   }
 }
